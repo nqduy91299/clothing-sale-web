5dde381e48ed9704563c02e86dd7a92981d18409
duynguyen <duynguyen@meu-solutions.com>
2021-04-22 12:03:50 +0700

    config prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
-  production: true,
+  production: false,
   ghnToken: 'ae567fa3-9bfc-11eb-9035-ae038bcc764b',
+  host: 'http://103.130.218.240:8080/',
+  hostImage: 'http://103.130.218.240:8080/public/images/',
 };
